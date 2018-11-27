22d68f343c360059d2b82a3213f052665da202ef
渣渣辉 <you@example.com>
2018-11-27 20:10:41 +0800

    提交

--- src/views/recommend/recommend.vue
+++ src/views/recommend/recommend.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="recommend">
-    <scroll :dataNum="dataNum" class="scroll">
-        <div class="slider-module" v-if="sliderData.length">
-          <slider :interval="2000" :autoPlay="true">
-            <slider-item v-for="obj in sliderData" :key="obj.id">
-              <a :href="obj.linkUrl">
-                <img @load="addDataNum()" :src="obj.picUrl" alt="">
-              </a>
-            </slider-item>
-          </slider>
-        </div>
-        <div class="list-module">
+    <scroll :dataNum="dataNum"
+            class="scroll"
+            :listenScroll="true"
+            :probeType="3"
+            @scroll="onScroll"
+            ref="scroll">
+      <div class="slider-module" v-if="sliderData.length">
+        <slider :interval="2000" :autoPlay="true">
+          <slider-item v-for="obj in sliderData" :key="obj.id">
+            <a :href="obj.linkUrl">
+              <img @load="addDataNum()" :src="obj.picUrl" alt="">
+            </a>
+          </slider-item>
+        </slider>
+      </div>
+      <div class="list-module" ref="list">
         <h1 class="list-title">热门歌单推荐</h1>
-          <song-list @addDataNum="addDataNum()"
+        <song-list @addDataNum="addDataNum()"
                      v-if="listData.length"
                      :listData="listData"></song-list>
-          <div class="loading-box" v-show="!listData.length">
-            <loading></loading>
-          </div>
+        <div class="loading-box" v-show="!listData.length">
+          <loading></loading>
+        </div>
       </div>
     </scroll>
+    <h1 class="list-title-fix" v-show="fixShow">热门歌单推荐</h1>
   </div>
 </template>
 
@@ -36,7 +42,9 @@
       return {
         sliderData: [],
         listData: [],
-        dataNum: 0
+        dataNum: 0,
+        scrollY: 0,
+        fixShow: false
       }
     },
     components: {
@@ -68,11 +76,21 @@
       },
       addDataNum(){
         this.dataNum++
+      },
+      onScroll(pos) {
+        this.scrollY = pos.y
       }
     },
     watch: {
       listData(){
         this.dataNum++
+      },
+      scrollY() {
+        if (-this.scrollY >= this.$refs.list.offsetTop) {
+          this.fixShow = true
+        } else {
+          this.fixShow = false
+        }
       }
     }
   }
@@ -90,14 +108,26 @@
       height: 100%
       overflow: hidden
       .list-module
+        position: relative
         .loading-box
           position: absolute
           width: 100%
           bottom: 0
           transform translateY(100%)
         .list-title
+          width: 100%
           color: $color-theme
           font-size: $font-size-medium
           text-align: center
           line-height: 65px
+    .list-title-fix
+      width: 100%
+      color: $color-theme
+      background-color: $color-background
+      font-size: $font-size-medium
+      text-align: center
+      line-height: 66px
+      position: fixed
+      top: 87px
+      left: 0
 </style>
